f47d713902ed2ef30a8ece222cf330240e4a7aeb
Anfariza <priza12324@gmail.com>
2026-04-02 05:37:02 +0700

    update tugas sesi-10

--- laravel_Bootcamp/routes/web.php
+++ laravel_Bootcamp/routes/web.php
@@ -6,3 +6,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/products', function () {
+    return view('welcome');
+});
+
+Route::get('/cart', function () {
+    return view('welcome');
+});
+
+Route::get('/checkout', function () {
+    return view('welcome');
+});
